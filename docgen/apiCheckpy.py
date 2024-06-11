6e00fa713a05e180f00e036f3f2107907dbaaf1d
ConnorBryntesonMain <CBrynteso001@CSBSJU.edu>
2024-06-11 10:47:10 -0500

    Cleaning up the file to make it look and work a bit better
    Working with a list of keyterms instead of a big if and then also removing the code that is not being used.

--- docgen/apiCheckpy.py
+++ docgen/apiCheckpy.py
@@ -2,10 +2,12 @@ import requests
 from transformers import pipeline, AutoTokenizer
 
 # Replace these variables with the appropriate values
-verison = 17.4
-repo_owner = "github-tools"
-repo_name = "github-release-notes"
+verison = 0.0
+repo_owner = ""
+repo_name = ""
 token = ""
+#This is the list of words used in the commit message to determine if it is a feature or a bug fix
+keywords = ["fix", "add", "remove", "update", "return"]
 
 # GitHub API URL for the repository commits
 commits_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
@@ -49,38 +51,6 @@ def get_commits_between(start_sha, end_sha):
 
     return all_commits[end_index:start_index+1]
 
-def commit_by_sha(sha, token=None):
-    commit_url = f"{commits_url}/{sha}"
-    headers = {}
-    if token:
-        headers['Authorization'] = f'token {token}'
-
-    response = requests.get(commit_url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to fetch commit {sha}: {response.status_code}, {response.text}")
-        return None
-        
-
-def get_commit_by_sha():
-    sha = input("Enter the SHA of the commit: ")
-    commit_details = get_commit_by_sha(sha, token=None)
-    if commit_details:
-        print(f"Commit SHA: {commit_details['sha']}")
-        print(f"Author: {commit_details['commit']['author']['name']}")
-        print(f"Date: {commit_details['commit']['author']['date']}")
-        print(f"Message: {commit_details['commit']['message']}")
-        print("Files changed:")
-        for file in commit_details.get("files", []):
-            print(f"File: {file['filename']}")
-            print("Changes:")
-            print(file['patch'])
-    else:
-        print("No details found for the given SHA.")
-        
-    return commit_details
-
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
 
@@ -98,36 +68,19 @@ def summarize_code(code):
 
     return summary
 
+# Function could be removed or made to look for something like the version so you dont have to add it but I think that would be more work than just adding the version
+
 def get_all_message_summary():
     commits = get_all_commits()
     for commit in commits:
-        commit_message = commit['message'].replace('\n', ' ')
-        commit_message = commit_message.replace('\r', ' ')
-        commit_message = commit_message.replace('\t', ' ')
-        commit_message = commit_message.replace('  ', ' ')
+        commit_message = ' '.join(commit['message'].split())
+        
         if(commit_message.startswith("Merge") or commit_message.startswith("Merge pull request")):
             continue
         summary = summarize_code(commit_message)
         print(f"Summary: {summary}")
 
 path= "docgen\output_test.md"
-
-"""
-def get_messages_by_sha(sha1, sha2):
-    commits = get_commits_between(sha1, sha2)
-    with open(path, 'w') as f:
-        for commit in commits:
-            commit_message = commit['message'].replace('\n', ' ')
-            commit_message = commit_message.replace('\r', ' ')
-            commit_message = commit_message.replace('\t', ' ')
-            commit_message = commit_message.replace('  ', ' ')
-            if(commit_message.startswith("Merge") or commit_message.startswith("Merge pull request") or "revert" in commit_message.lower()):
-                continue
-            summary = summarize_code(commit_message)
-            if "CNN" in summary:
-                continue
-            f.write(f"Summary: {summary}\n" + commit['sha'] + "\n")
-"""
 
 def release_notes(sha1, sha2, Version):
     commits = get_commits_between(sha1, sha2)
@@ -136,31 +89,19 @@ def release_notes(sha1, sha2, Version):
         f.write(f"Release Notes Version {Version}\n\n")
         
         for commit in commits:
-            commit_message = commit['message'].replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').replace('  ', ' ')
+            commit_message = ' '.join(commit['message'].split())
             
             if(commit_message.startswith("Merge") or commit_message.startswith("Merge pull request")):
                 continue
             
-            if("fix" in commit_message.lower() or "add" in commit_message.lower() or "remove" in commit_message.lower() or "update" in commit_message.lower() or "return" in commit_message.lower()):
+            if any(keyword in commit_message for keyword in keywords):
                 summary = summarize_code(commit_message)
-                
+                # I need to make it do a better job of filtering out and summarizing the commits because it is not doing a good job that is where we get CNN
                 if "CNN" in summary:
                     continue
                 url = f"https://github.com/{repo_owner}/{repo_name}/commit/"
                 f.write(f"Commit Summary: {summary}\n" + url+commit['sha'] + "\n\n")
-    
-
-# Main function to call the above functions
-
-def main():
-    #print("Code summary:")
-    #get_all_message_summary() 
-    
-    #print("Commits between two SHAs:")
-    #get_messages_by_sha('5429f72eedd269303cadc7394054e69b442ed7cc', '114602d458d1034faa52b71e4c15aba9b3a17698')
-    
-    release_notes("62c9289306d4b9abd7170d0fbdf29b25a4dd112c", "ff0ddae3938db3cfcca5ef317d74eee034c32cc5", verison)
 
 
 if __name__ == "__main__":
-    main()
+    release_notes("", "", verison)
